b7b121aa7e41386b6be618a0d817d017e6ddac29
zalew <zalew7@gmail.com>
2011-04-11 08:56:45 +0200

    FAV_REMOVE message bug

--- favorites/settings.py
+++ favorites/settings.py
@@ -2,7 +2,7 @@ from django.conf import settings
 from django.utils.translation import ugettext
 
 FAV_ADD = getattr(settings, 'FAV_ADD', ugettext('like')) 
-FAV_REMOVE = getattr(settings, 'FAV_ADD', ugettext('unlike'))
+FAV_REMOVE = getattr(settings, 'FAV_REMOVE', ugettext('unlike'))
 FAV_COUNT_SINGLE = getattr(settings, 'FAV_COUNT_SINGLE', ugettext('%d person likes this'))
 FAV_COUNT_PLURAL = getattr(settings, 'FAV_COUNT_PLURAL', ugettext('%d people like this'))
 # for languages who destinguish another plural form, such as Polish if the last digit is 2
